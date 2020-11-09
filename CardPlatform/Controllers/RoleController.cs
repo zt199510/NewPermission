bc3d24674a4f7fbce00375852964be27b4988318
田 朱 <1033566918@qq.com>
2020-11-09 17:18:48 +0800

    添加角色接口

--- CardPlatform/Controllers/RoleController.cs
+++ CardPlatform/Controllers/RoleController.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CardPlatform.Common;
 using Microsoft.AspNetCore.Mvc;
 using ZtApplication.RoleApp;
+using ZtApplication.RoleApp.Dtos;
 
 namespace CardPlatform.Controllers
 {
@@ -15,5 +17,44 @@ namespace CardPlatform.Controllers
         {
             _service = service;
         }
+
+        /// <summary>
+        /// 新增或编辑功能
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        public IActionResult Edit(RoleDto dto)
+        {
+            var Res = new ServiceResult();
+            Res.IsFailed();
+            if (!ModelState.IsValid)
+            { 
+                return Ok(Res);
+            }
+            if (dto.Id == Guid.Empty)
+                dto.CreateTime = DateTime.Now;
+           
+            if (_service.InsertOrUpdate(dto))
+            {
+                Res.IsSuccess();
+                return Ok(Res);
+            }
+            return Ok(Res);
+        }
+
+        public IActionResult GetAllPageList(int startPage, int pageSize)
+        {
+            int rowCount = 0;
+            var result = _service.GetAllPageList(startPage, pageSize, out rowCount);
+            return Ok(new
+            {
+                rowCount = rowCount,
+                pageCount = Math.Ceiling(Convert.ToDecimal(rowCount) / pageSize),
+                rows = result,
+            });
+        }
+
+
+
     }
 }
